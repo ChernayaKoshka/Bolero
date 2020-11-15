3c339e586c463aba3d6235ab880f96cbf6c3686d
FatherFoxxy <rattusunison@gmail.com>
2020-11-14 17:30:47 -0800

    Support optional types in QueryStringSerializer

--- src/Bolero/RemotingSerializers/QueryStringSerializer.fs
+++ src/Bolero/RemotingSerializers/QueryStringSerializer.fs
@@ -1,4 +1,5 @@
 module Bolero.Remoting.QueryStringSerializer
+
 open System
 open System.Reflection
 open FSharp.Reflection
@@ -12,14 +13,40 @@ let internal getRecordFields (recordType: Type) =
     else
         Error "Only records/unit are supported for query string (de)serialization."
 
+let internal isOptionType (typ: Type) =
+    typ.IsGenericType && typ.GetGenericTypeDefinition() = typedefof<Option<_>>
+
+let internal isTypeSerializable (typ: Type) =
+    let rec checkType (checkingOptionCase: bool) (typ: Type) =
+        if typ.IsPrimitive || typ = typeof<string> then
+            true
+        // keeping code block like this for clarity (hopefully)
+        // fsharplint:disable-next-line Hints
+        else if
+            // don't want to return a false positive on a type like Option<Option<string>>
+            not checkingOptionCase
+            && isOptionType typ
+            && checkType true (typ.GetGenericArguments().[0]) then
+            true
+        else
+            false
+    checkType false typ
+
 let internal areConstraintsSatisfied (recordType: Type) (props: PropertyInfo array) =
     let satisfied =
         props
-        |> Array.forall (fun prop ->
-            prop.PropertyType.IsPrimitive || prop.PropertyType = typeof<string>
+        |> Array.map (fun prop ->
+            (prop, isTypeSerializable prop.PropertyType)
         )
-    if satisfied then Ok props
-    else Error (sprintf "All properties of type '%s' must be primitives." recordType.AssemblyQualifiedName)
+    if Array.forall (snd >> (=) true) satisfied then
+        Ok props
+    else
+        satisfied
+        |> Array.filter (snd >> (=) false)
+        |> Array.map (fun (prop, _) -> prop.Name)
+        |> String.concat ", "
+        |> sprintf "All properties of type '%s' must be primitives or string. Offending properties as follows: %s" recordType.AssemblyQualifiedName
+        |> Error
 
 // TODO: Purely for organization, could be moved up?
 module private Serialize =
@@ -31,10 +58,25 @@ module private Serialize =
         |> String.concat "&"
         |> sprintf "?%s"
 
+    let extractOptionValue (instance: obj) (prop: PropertyInfo) =
+        let value = prop.GetValue(instance)
+        if isNull value then
+            None
+        else
+            FSharpValue.GetUnionFields(value, prop.PropertyType)
+            |> snd
+            |> Array.head
+            |> Some
+
     let extractPropertyValues (instance: obj) (props: PropertyInfo array) =
         props
-        |> Array.map (fun prop ->
-            (prop.Name, string (prop.GetValue(instance)))
+        |> Array.choose (fun prop ->
+            if isOptionType prop.PropertyType then
+                prop
+                |> extractOptionValue instance
+                |> Option.map (fun value -> (prop.Name, string value))
+            else
+                Some (prop.Name, string (prop.GetValue(instance)))
         )
 
     let serialize (toSerialize: obj) =
@@ -58,7 +100,18 @@ module private Deserialize =
             let recordFields =
                 props
                 |> Array.map (fun prop ->
-                    Convert.ChangeType(valueMap.[prop.Name], prop.PropertyType)
+                    match Map.tryFind prop.Name valueMap, isOptionType prop.PropertyType with
+                    | Some value, true ->
+                        let someCase =
+                            FSharpType.GetUnionCases(prop.PropertyType)
+                            |> Array.find (fun uc -> uc.Name = "Some")
+                        FSharpValue.MakeUnion(someCase, [| Convert.ChangeType(value, prop.PropertyType.GetGenericArguments().[0]) |])
+                    | Some value, false ->
+                        Convert.ChangeType(value, prop.PropertyType)
+                    | None, true ->
+                        box None
+                    | None, false ->
+                        raise (System.Collections.Generic.KeyNotFoundException("The given key was not present in the dictionary."))
                 )
             FSharpValue.MakeRecord(t, recordFields)
             :?> 'T
